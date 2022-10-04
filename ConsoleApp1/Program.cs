30d16a71a3077c29a314f069876bf5935bacf1a8
Nikita Smetannikov <smetannikovnikita@yandex.ru>
2022-10-04 11:23:00 +0300

    Task resolved

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] array = {"hello", "2", "world", ":-)", "123", "1"};
+
+string[] FilterArray(string[] arr)
+{
+    string[] processedArray = new string[arr.Length];
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            processedArray[counter] = arr[i];
+            counter++;
+        }
+    }
+
+    return processedArray;
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+PrintArray(FilterArray(array));
